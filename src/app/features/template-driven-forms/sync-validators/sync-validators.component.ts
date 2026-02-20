80e82eb34a8a35992b36c2dd04b59d78c6dbceb6
Henrique Custódia <martinshenrique8@gmail.com>
2026-02-20 16:24:44 -0300

    feat: finaliza exemplos com validações prontas do Angular, consumo da instância de ngModel no template e renderização de mensagens customizadas

--- src/app/features/template-driven-forms/sync-validators/sync-validators.component.ts
+++ src/app/features/template-driven-forms/sync-validators/sync-validators.component.ts
@@ -1,12 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sync-validators',
-  imports: [],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './sync-validators.component.html',
   styleUrl: './sync-validators.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SyncValidatorsComponent {
+
+  protected message = signal('');
 
 }
