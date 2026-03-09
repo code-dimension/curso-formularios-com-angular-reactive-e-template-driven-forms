f3e314762784df4e7c1969629fa1d22b653e073c
Henrique Custódia <martinshenrique8@gmail.com>
2026-03-09 14:13:25 -0300

    feat: aplica erros manualmente

--- src/app/features/reactive-forms/routes.ts
+++ src/app/features/reactive-forms/routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { FormControlComponent } from './1.form-control/form-control.component';
 import { ValidationsComponent } from './2.validations/validations.component';
 import { UsingMarkFunctionsComponent } from './3.using-mark-functions/using-mark-functions.component';
+import { SetErrorsComponent } from './4.set-errors/set-errors.component';
 
 export const reactiveFormsRoutes: Routes = [
   {
@@ -15,5 +16,9 @@ export const reactiveFormsRoutes: Routes = [
   {
     path: 'using-mark-functions',
     component: UsingMarkFunctionsComponent,
+  },
+  {
+    path: 'set-errors',
+    component: SetErrorsComponent,
   },
 ];
